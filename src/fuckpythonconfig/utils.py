5775bc758dc95319d188bb56c5b7fb1076dcf02e
JGG0sbp66 <2791456537@qq.com>
2025-09-26 19:55:49 +0800

    做了ruff检查，修正了一些格式问题，抛出异常时使用from e来获取上下文异常

--- src/fuckpythonconfig/utils.py
+++ src/fuckpythonconfig/utils.py
@@ -1,22 +1,28 @@
 # 存放各种读取工具
-import re
 import os
+import re
 import tomllib
 
-from .exceptions import FileNotFoundError
+from .exceptions import FileNotFoundError, TOMLReadError
 
-ENV_VAR_REGEX = r'\$\{[^}]*\}'
+ENV_VAR_REGEX = r"\$\{[^}]*\}"
+
 
 def is_env_var(value: str) -> bool:
     """使用正则表达式判断是否为${value}格式"""
     return bool(re.fullmatch(ENV_VAR_REGEX, value))
 
+
 def read_toml(file_path: str) -> dict:
     """使用tomllib读取TOML文件"""
     if not os.path.exists(file_path):
-        raise FileNotFoundError("Can not find TOML file", file_path, None)
-    with open(file_path, "rb") as f:
-        return tomllib.load(f)
+        raise FileNotFoundError("Can not find TOML file", file_path)
+    try:
+        with open(file_path, "rb") as f:
+            return tomllib.load(f)
+    except Exception as e:
+        raise TOMLReadError("Failed to read TOML file", file_path) from e
+
 
 def get_env_var(env_key: str) -> str:
     """使用os读取环境变量"""
